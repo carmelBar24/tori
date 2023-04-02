df8fbcc20b61185953055aa83c41b0751332c7b0
Alon Vaisgur <37458479+Romeore@users.noreply.github.com>
2023-04-02 11:03:59 +0300

    [Remove] Deleted old requests page

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ import 'filter_page.dart';
 import 'future_turns_page.dart';
 import 'home_page.dart';
 import 'login_page.dart';
-import 'received_requests_page_new.dart';
+import 'received_requests_page.dart';
 import 'sent_requests_page.dart';
 import 'swap_page.dart';
 
@@ -48,6 +48,6 @@ class ToriApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Tori', home: ReceivedRequestsPageNew());
+    return MaterialApp(title: 'Tori', home: HomePage());
   }
 }
